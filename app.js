746ec7967e4843c8ee314b05657938ca4d6ad794
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-02-20 15:43:34 -0800

    added admin route

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // Import the express module
 import express from 'express';
 
+//array that will store submitted form data
+let contacts = [];
 
 // Create an instance of an Express application
 const app = express();
 
 app.use(express.static('public'));
+
+//middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
 
 
 // Define the port number where our server will listen
@@ -19,6 +24,25 @@ app.get('/', (req, res) => {
     
     res.sendFile(`${import.meta.dirname}/views/index.html`);
 });
+
+// Handle form submission
+app.post('/submit', (req, res) => {
+    console.log("New submission:", req.body);
+    contacts.push(req.body);
+    res.redirect('/confirmation');
+});
+
+
+// Confirmation page
+app.get('/confirmation', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+// admin route
+app.get('/admin', (req, res) => {
+    res.json(contacts); // send the array as JSON
+});
+
 
 
 
